311e52f6c99448b445621f9340a3bcd158e8ed63
Neboola <r.v.bykova@mail.ru>
2016-01-23 20:20:25 +0700

    23 01 16

---
+++ src/com/javarush/test/level18/lesson08/task03/MySolution.java
@@ -0,0 +1,7 @@
+package com.javarush.test.level18.lesson08.task03;
+
+/**
+ * Created by OmniTool on 23.01.2016.
+ */
+public class MySolution {
+}
---
+++ src/com/javarush/test/level18/lesson08/task04/MySolution.java
@@ -0,0 +1,7 @@
+package com.javarush.test.level18.lesson08.task04;
+
+/**
+ * Created by OmniTool on 23.01.2016.
+ */
+public class MySolution {
+}
